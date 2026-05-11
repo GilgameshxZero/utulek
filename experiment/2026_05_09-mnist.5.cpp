150f9ddec23656d4623c89dceaa945a41ab05d74
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2026-05-11 03:34:04 +0000

    working network single backprop on mnist

--- experiment/2026_05_09-mnist.5.cpp
+++ experiment/2026_05_09-mnist.5.cpp
@@ -7,6 +7,7 @@ using namespace Algorithm;
 using namespace Data;
 using namespace Math;
 using namespace Error;
+using namespace Neural;
 using namespace std;
 
 using LL = long long;
@@ -56,7 +57,7 @@ int main(int, char const *const *const) {
 		cout << z1 << endl << z2 << endl << gradient << endl;
 	}
 	{
-		Tensor<LD, 1> z1({{{4}}, 1.0L, -2.0L, 3.0L, -4.0L});
+		Tensor<LD, 1> z1({{4}, 1.0L, -2.0L, 3.0L, -4.0L});
 		Neural::Activation::Linear<LD> activation(
 			Tensor<LD, 2>::identity(4),
 			Tensor<LD, 1>({4}, -1.0L, -1.0L, 1.0L, 1.0L));
@@ -73,6 +74,80 @@ int main(int, char const *const *const) {
 		auto z2{activation.asApply(z1)};
 		auto gradient{activation.getGradient(z2)};
 		cout << z1 << endl << z2 << endl << gradient << endl;
+	}
+
+	{
+		random_device rd;
+		mt19937 gen(0);
+		uniform_real_distribution<LD> dist;
+
+		Network::FeedForward<LD> network(
+			{make_shared<Activation::Linear<LD>>(
+				 Tensor<LD, 2>({8, 16}, gen, dist),
+				 Tensor<LD, 1>({16}, gen, dist)),
+				make_shared<Activation::Relu<LD>>(),
+				make_shared<Activation::Linear<LD>>(
+					Tensor<LD, 2>({16, 4}, gen, dist),
+					Tensor<LD, 1>({4}, gen, dist)),
+				make_shared<Activation::Softmax<LD>>()});
+		Tensor<LD, 1> x(
+			{8},
+			1.0L,
+			-2.0L,
+			3.0L,
+			-4.0L,
+			5.0L,
+			-6.0L,
+			7.0L,
+			-8.0L),
+			y({4}, 0.0L, 0.0L, 1.0L, 0.0L);
+		auto artifact{network.asApplyWithArtifact(x)};
+		cout << artifact << endl;
+		auto yHat{artifact.back()};
+		cout << x << endl << yHat << endl << y << endl;
+		auto const l{Loss::CrossEntropy<LD>(y)};
+		auto loss{l.asApply(yHat)};
+		// d(loss = l_0) / d(z_4) \in R^{n_4 = c = 4}.
+		auto g4{l.getGradient(yHat)};
+		cout << loss << endl << g4 << endl;
+		// d(loss = l_0) / d(z_3) \in R^{n_3 = 4}.
+		auto g3{g4.asMultiply<1>(
+			network.layer[3].get()->getGradient(artifact[4]),
+			{0},
+			{0})};
+		cout << g3 << endl;
+		releaseAssert(g3.size() == array<size_t, 1>{{4}});
+		// d(loss = l_0) / d(z_2) \in R^{n_2 = 16}.
+		auto g2{g3.asMultiply<1>(
+			network.layer[2].get()->getGradient(artifact[3]),
+			{0},
+			{0})};
+		cout << g2 << endl;
+		releaseAssert(g2.size() == array<size_t, 1>{{16}});
+		// d(loss = l_0) / d(z_1) \in R^{n_1 = 16}.
+		auto g1{g2.asMultiply<1>(
+			network.layer[1].get()->getGradient(artifact[2]),
+			{0},
+			{0})};
+		cout << g1 << endl;
+		releaseAssert(g1.size() == array<size_t, 1>{{16}});
+		// d(loss = l_0) / d(z_0 = x) \in R^{n_0 = 8}.
+		auto g0{g1.asMultiply<1>(
+			network.layer[0].get()->getGradient(artifact[1]),
+			{0},
+			{0})};
+		cout << g0 << endl;
+		releaseAssert(g0.size() == array<size_t, 1>{{8}});
+
+		auto gradient{network.getGradient(l, artifact)};
+		cout << gradient << endl;
+
+		RF(i, 0, 256) {
+			network.stepWithGradient(l, artifact, 1e-3);
+			auto yHatMod{network.asApply(x)};
+			auto loss{l.asApply(yHatMod)};
+			cout << yHatMod << endl << loss << endl;
+		}
 	}
 	return 0;
 }
---
+++ experiment/2026_05_09-mnist.6.cpp
@@ -0,0 +1,117 @@
+#include <rain.hpp>
+
+#include "platform.hpp"
+
+using namespace Rain;
+using namespace Algorithm;
+using namespace Data;
+using namespace Math;
+using namespace Error;
+using namespace Neural;
+using namespace std;
+
+using LL = long long;
+using LD = long double;
+
+#define RF(x, from, to)                                    \
+	for (                                                    \
+		LL x(from), _to(to), _delta{x < _to ? 1LL : -1LL};     \
+		x != _to;                                              \
+		x += _delta)
+size_t constexpr C_CLASSES{10};
+LD STEP_SIZE{1e-3};
+
+template<typename Value>
+char pixelToChar(Value pixel) {
+	if (pixel < 0x80) {
+		return pixel < 0x40 ? ' ' : '-';
+	} else {
+		return pixel < 0xc0 ? '=' : '#';
+	}
+}
+
+template<typename Value>
+void showImg(
+	Tensor<Value, 2> const &x,
+	ostream &stream = cout) {
+	x.template applyOver<1>([&](Tensor<Value, 1> const &row) {
+		row.template applyOver<0>([&](Value const &pixel) {
+			auto pixelChar{pixelToChar(pixel)};
+			stream << pixelChar << pixelChar;
+		});
+		stream << '\n';
+	});
+}
+
+int main(int, char const *const *const) {
+	auto assetPath{getAssetPath(__FILE__)};
+	cout << "Asset path: " << assetPath << '.' << endl;
+
+	random_device rd;
+	mt19937 gen(0);
+	uniform_real_distribution<LD> dist;
+
+	Tensor<uint8_t, 3> trainX, testX;
+	Tensor<uint8_t, 1> trainY, testY;
+	{
+		ifstream fStream(assetPath / ".data/mnist.hfm");
+		HuffmanStreamBuf decoderBuf(*fStream.rdbuf());
+		Deserializer deserializer(&decoderBuf);
+		deserializer >> trainX >> trainY >> testX >> testY;
+	}
+	auto trainXDbl{trainX.asReshape<2>({trainX.size()[0], 0})
+			.asRetype<LD>()},
+		testXDbl{trainX.asReshape<2>({testX.size()[0], 0})
+				.asRetype<LD>()};
+	Tensor<LD, 2> trainYOneHot({trainY.size()[0], C_CLASSES}),
+		testYOneHot({testY.size()[0], C_CLASSES});
+	trainYOneHot.applyOver<1>(
+		[&](Tensor<LD, 1> &left, uint8_t const &right) {
+			left[right] = 1;
+		},
+		trainY);
+	testYOneHot.applyOver<1>(
+		[&](Tensor<LD, 1> &left, uint8_t const &right) {
+			left[right] = 1;
+		},
+		testY);
+
+	{
+		Network::FeedForward<LD> network(
+			{make_shared<Activation::Linear<LD>>(
+				 Tensor<LD, 2>({784, 64}, gen, dist),
+				 Tensor<LD, 1>({64}, gen, dist)),
+				make_shared<Activation::Relu<LD>>(),
+				make_shared<Activation::Linear<LD>>(
+					Tensor<LD, 2>({64, 10}, gen, dist),
+					Tensor<LD, 1>({10}, gen, dist)),
+				make_shared<Activation::Normalization<LD>>(),
+				make_shared<Activation::Softmax<LD>>()});
+
+		RF(j, 0, 64) {
+			RF(i, 0, 4096) {
+				auto x{trainXDbl[i]}, y{trainYOneHot[i]};
+				// cout << y << endl;
+				Loss::CrossEntropy<LD> l(y);
+				auto artifact{network.asApplyWithArtifact(x)};
+				// cout << artifact << endl;
+				// cout << artifact.back() << endl;
+				cout << l.asApply(artifact.back()) << endl;
+				network.stepWithGradient(l, artifact, STEP_SIZE);
+			}
+			STEP_SIZE *= 0.99;
+		}
+		cout << STEP_SIZE << endl;
+
+		RF(i, 59995, 60000) {
+			auto x{trainXDbl[i]}, y{trainYOneHot[i]};
+			showImg(x.asReshape<2>({28, 28}));
+			cout << y << endl;
+			Loss::CrossEntropy<LD> l(y);
+			auto artifact{network.asApplyWithArtifact(x)};
+			cout << artifact.back() << endl;
+			cout << l.asApply(artifact.back()) << endl;
+		}
+	}
+	return 0;
+}
